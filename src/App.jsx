dfb304292af5f6490e61709fcc7ad06f5fa30579
NischalBhatta <nischalpb@gmail.com>
2026-08-27 21:31:37 +1000

    Added dummy dataset to show active and archived habit list

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,83 @@
 import "./App.css";
-import HabitList from "./components/habitList.jsx";
-import ArchiveList from "./components/ArchiveList.jsx";
+import HabitList from "./components/HabitList.jsx";
+// import Streak from "./components/Streak.jsx";
+
+import React, { useState } from "react";
 
 function App() {
+  const [habitList, setHabitList] = useState([
+    {
+      _id: "1",
+      name: "Drink 2L of water",
+      type: "active",
+      isCompleted: true,
+    },
+    {
+      _id: "2",
+      name: "Read 20 minutes",
+      type: "active",
+      isCompleted: false,
+    },
+    {
+      _id: "3",
+      name: "Drink 2L of water",
+      type: "active",
+      isCompleted: true,
+    },
+    {
+      _id: "4",
+      name: "Read 20 minutes",
+      type: "active",
+      isCompleted: false,
+    },
+  ]);
+
+  const activeHabits = habitList.filter((habit) => habit.isCompleted);
+  const archiveHabits = habitList.filter((habit) => !habit.isCompleted);
+
+  const handleArchiveToggle = (id) => {
+    setHabitList((currentList) =>
+      currentList.map((habit) =>
+        habit._id === id
+          ? {
+              ...habit,
+              isArchived: !habit.isArchived,
+            }
+          : habit,
+      ),
+    );
+  };
+
+  const handleDeleteButton = (id) => {
+    if (window.confirm("Are you sure you want to delete this habit?")) {
+      setHabitList((currentList) =>
+        currentList.filter((habit) => habit._id !== id),
+      );
+    }
+  };
   return (
     <>
       <div className="wrapper">
         <div className="container p-2">
-          {/* Title */}
-          <h2 className="text-center p-2">Habit Tracker</h2>
-          <div className="row">
-            {/* Habit List */}
-            <div className="col-6">
-              <HabitList />
+          {/* Banner */}
+          <header className="banner">
+            <div className="app-title">
+              <h2>Habit Tracker</h2>
             </div>
-            <div className="col-6">
-              {/* Archive List */}
-              <ArchiveList />
+            <div className="add-habit">
+              <button className="add-habit-btn">ADD HABIT</button>
             </div>
-          </div>
+          </header>
 
-          {/* Streak/HeatMap */}
+          {/* Habit List */}
+          <HabitList
+            activeHabits={activeHabits}
+            archiveHabits={archiveHabits}
+            handleArchiveToggle={handleArchiveToggle}
+          />
+
+          {/* Habit Streak  */}
+          {/* <Streak /> */}
         </div>
       </div>
     </>
--- src/components/habitList.jsx
+++ src/components/habitList.jsx
@@ -1,11 +1,87 @@
 import React, { useState } from "react";
 
-const habitList = () => {
+export const HabitList = ({
+  activeHabits,
+  archivedHabits,
+  handleToggleComplete,
+  handleArchiveToggle,
+  handleDelete,
+}) => {
   return (
-    <>
-      <h3>Your Daily Habit</h3>
-    </>
+    <div className="habit-list">
+      {/* active habits */}
+      <div className="active-table">
+        <div className="table-header">
+          <h2>Active Habits</h2>
+
+          <hr />
+        </div>
+        <div>
+          <table className="active-list">
+            {activeHabits.map((item, i) => (
+              <tbody>
+                <tr key={item._id}>
+                  <td>{i + 1}</td>
+                  <td>
+                    <button
+                      type="checkbox"
+                      className="handleToggleButton"
+                      value={item._id}
+                      id=""
+                    ></button>
+                  </td>
+                  <td>{item.name}</td>
+                  <td>
+                    <button className="handleArchiveButton">Archive</button>
+                  </td>
+                  <td>
+                    <button className="handleArchiveButton">Delete</button>
+                  </td>
+                </tr>
+                <tr></tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+      </div>
+
+      {/* archived habits */}
+      <div className="active-table">
+        <div className="table-header">
+          <h2>Active Habits</h2>
+
+          <hr />
+        </div>
+        <div>
+          <table className="active-list">
+            {activeHabits.map((item, i) => (
+              <tbody>
+                <tr key={item._id}>
+                  <td>{i + 1}</td>
+                  <td>
+                    <button
+                      type="checkbox"
+                      className="handleToggleButton"
+                      value={item._id}
+                      id=""
+                    ></button>
+                  </td>
+                  <td>{item.name}</td>
+                  <td>
+                    <button className="handleArchiveButton">Archive</button>
+                  </td>
+                  <td>
+                    <button className="handleArchiveButton">Delete</button>
+                  </td>
+                </tr>
+                <tr></tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default habitList;
+export default HabitList;
